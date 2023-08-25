b9d41588165437e6049abfafde983384281b0569
Samuel da Silva Santos <samuel.santos@topaziopartner.com.br>
2023-08-25 15:41:29 -0300

    Navbar inserted Links

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,9 +4,9 @@ export function Navbar() {
     return (
         <nav>
             <ul className="container flex gap-2 mt-10">
-                <li><a href="" className="h-10 w-10 flex justify-center items-center bg-slate-900 text-slate-300 hover:bg-slate-800 rounded-lg" aria-label="Github"><Github className='w-4'/></a></li>
-                <li><a href="" className="h-10 w-10 flex justify-center items-center bg-slate-900 text-slate-300 hover:bg-slate-800 rounded-lg" aria-label="Github"><Linkedin className='w-4'/></a></li>
-                <li><a href="" className="h-10 w-10 flex justify-center items-center bg-slate-900 text-slate-300 hover:bg-slate-800 rounded-lg" aria-label="Github"><Twitter className='w-4'/></a></li>
+                <li><a href="https://github.com/" className="h-10 w-10 flex justify-center items-center bg-slate-900 text-slate-300 hover:bg-slate-800 rounded-lg" aria-label="Github"><Github className='w-4'/></a></li>
+                <li><a href="https://www.linkedin.com/in/samuelleao/" className="h-10 w-10 flex justify-center items-center bg-slate-900 text-slate-300 hover:bg-slate-800 rounded-lg" aria-label="Github"><Linkedin className='w-4'/></a></li>
+                <li><a href="https://twitter.com/Samuelleao_" className="h-10 w-10 flex justify-center items-center bg-slate-900 text-slate-300 hover:bg-slate-800 rounded-lg" aria-label="Github"><Twitter className='w-4'/></a></li>
             </ul>
         </nav>
     )
